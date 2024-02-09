1c03312416c52f7a471934dd897d87ac81ac72db
KT-TJS <kevin.trepanier.1@ens.etsmtl.ca>
2024-02-08 19:36:22 -0500

    Front-end 1.0

--- ressources/main.js
+++ ressources/main.js
@@ -1,21 +1,82 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener("DOMContentLoaded", function () {
 
-    let stationsList=document.querySelectorAll(".stationList>li");
-    console.log(stationsList);
+    //Évènement lancé lorsqu'une station est sélectionnée
+    const selectStationCE = new CustomEvent("selectStation", {
+        bubbles: true,
+        detail: {}
+    });
+    let infoHolder = new InfoHolder();
+    infoHolder.init();
 
-    for(let i=0;i<stationsList.length;i++){
+    let stationsList = document.querySelectorAll(".stationList>li");
 
-        stationsList[i].addEventListener("mouseenter",function(){
+    for (let i = 0; i < stationsList.length; i++) {
 
-           stationsList[i].classList.add("open");
+        stationsList[i].querySelector("span").addEventListener("click", function () {
+            document.querySelectorAll(".active").forEach(function (item) {
+
+                item.classList.remove("active");
+
+            });
+            stationsList[i].classList.add("active");
 
         });
+        let stationsSubList = stationsList[i].querySelectorAll(".stationSubList>li");
 
-        stationsList[i].addEventListener("mouseout",function(){
+        for (let j = 0; j < stationsSubList.length; j++) {
 
-            stationsList[i].classList.remove("open");
+            stationsSubList[j].addEventListener("selectStation", selectStation);
+            stationsSubList[j].addEventListener("click", function () {
 
-        });
+                let activeToRemove = document.querySelectorAll(".active");
+                activeToRemove.forEach(function (item) {
+                    if (item !== stationsSubList[j]) {
+                        item.classList.remove("active");
+                    }
+                });
+                stationsSubList[j].classList.toggle("active");
+                stationsList[i].classList.add("active");
+                dispatchEvent(selectStationCE);
+
+            });
+
+        }
 
     }
+
+    function selectStation() {
+
+        //write code for data insertion here
+
+    }
+
 });
+
+/*Toggle data-type-area*/
+
+class InfoHolder {
+
+    constructor() {
+
+        this.infoHolderElement = document.querySelector(".infoHolder");
+        this.buttonNavList = document.querySelectorAll(".stationDataNavButton");
+
+
+    }
+
+    changeToView(areaType = "Data") {
+        this.infoHolderElement.setAttribute("data-type-area", areaType);
+    }
+
+    init() {
+
+        this.buttonNavList.forEach(function (item) {
+
+            item.addEventListener("click", function (e) {
+                this.changeToView(e.target.getAttribute("data-type-view"));
+            }.bind(this));
+        }.bind(this));
+
+    }
+
+}
