c234870cb7bb0b0cf67fcb7542e700b7b049803a
boubou243 <36116936+boubou243@users.noreply.github.com>
2024-02-10 18:24:37 -0500

    debut

--- ressources/main.js
+++ ressources/main.js
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+    let start=0;
+    let fin=11;
+
     //Évènement lancé lorsqu'une station est sélectionnée
     const selectStationCE = new CustomEvent("selectStation", {
         bubbles: true,
@@ -13,10 +16,13 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let i = 0; i < stationsList.length; i++) {
 
         stationsList[i].querySelector("span").addEventListener("click", function () {
+           // document.getElementById("stationInfoTitle").innerHTML =   document.getElementsByClassName("active")[0].children[0].innerText;
             document.querySelectorAll(".active").forEach(function (item) {
 
                 item.classList.remove("active");
-
+                DisplayMonthsStart();
+               
+                
             });
             stationsList[i].classList.add("active");
 
@@ -27,6 +33,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
             stationsSubList[j].addEventListener("selectStation", selectStation);
             stationsSubList[j].addEventListener("click", function () {
+                document.getElementById("stationInfoTitle").innerHTML =   stationsSubList[j].innerText;
+
+               
 
                 let activeToRemove = document.querySelectorAll(".active");
                 activeToRemove.forEach(function (item) {
@@ -49,8 +58,58 @@ document.addEventListener("DOMContentLoaded", function () {
         //write code for data insertion here
 
     }
+    var select = document.getElementById("stationStartTimeMonth");
+
+    var select2 = document.getElementById("stationEndTimeMonth");
+    select.addEventListener("change",function(){
+
+
+            var selectedMonth= select.optiions[select.selectedIndex];
+            start= 4;
+            DisplayMonthsStart();
+            console.log(start);
+
+    });
+
+
+    function DisplayMonthsStart(){
+
+            select.innerHTML = "";
+
+          
+            select2.innerHTML = "";
+
+
+            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+            for( var i= start; i <= fin; i++){
+
+                    var nouvMois = document.createElement("option");
+                    nouvMois.value = i+1;
+                    nouvMois.text = months[i];
+                    select.appendChild(nouvMois);
+
+                    var nouvMois2 = document.createElement("option");
+                nouvMois2.value = i+1;
+                nouvMois2.text = months[i];
+                select2.appendChild(nouvMois2);
+
+            }
+
+
+
+
+    }
+
+ 
+
 
 });
+
+
+
+
+
 
 /*Toggle data-type-area*/
 
