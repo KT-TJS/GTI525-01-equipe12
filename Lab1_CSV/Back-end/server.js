da891d715cc1d33eaea32c6e6d1437c7287f5608
boubou243 <36116936+boubou243@users.noreply.github.com>
2024-03-05 17:30:35 -0500

    T1

--- Lab1_CSV/Back-end/server.js
+++ Lab1_CSV/Back-end/server.js
@@ -17,15 +17,20 @@ async function loadStationMapping() {
     const data = await fs.readFile('station_mapping.json', 'utf8');
     stationMappingData = JSON.parse(data);
     console.log('Station mapping data loaded successfully.');
+    console.log(4)
+    console.log(typeof stationMappingData)
+    console.log(stationMappingData)
+
   } catch (error) {
     console.error('Error loading station mapping data:', error);
   }
+
 }
 
 // Call the function to load station mapping data when the server starts
 loadStationMapping();
 
-const cityCode = '1865'; // Replace with the desired city code
+const cityCode = 'qc-133'; // Replace with the desired city code
 fetchWeatherData(cityCode)
   .then(data => {
     console.log('Weather data:', data);
@@ -33,6 +38,19 @@ fetchWeatherData(cityCode)
   .catch(error => {
     console.error('Error:', error.message);
   });
+
+  const station = '51457'; // Replace with the desired stationId
+  const annee = '2023'
+  const mois = '03'
+  const jour = '05'
+  fetchClimateData(station,annee,mois,jour)
+  .then(data => {
+    console.log('Weather aujourdhui:', data);
+  })
+  .catch(error => {
+    console.error('Error:', error.message);
+  });
+
 
 // Route to handle AJAX requests
 app.post('/ajax_endpoint', async (req, res) => {
@@ -53,7 +71,7 @@ app.post('/ajax_endpoint', async (req, res) => {
   }
 });
 
-
+//Retourne prévision pour la journée actuelle
 async function fetchWeatherData(code) {
   try {
     const rssUrl = `https://meteo.gc.ca/rss/city/${code}_f.xml`;
@@ -64,6 +82,19 @@ async function fetchWeatherData(code) {
     throw new Error('Failed to fetch weather data');
   }
 }
+
+async function fetchClimateData(stationId, year, month, day) {
+  try {
+      const climateUrl = `https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=${stationId}&Year=${year}&Month=${month}&Day=${day}&timeframe=1&submit=%20Download+Data`;
+      const response = await fetch(climateUrl);
+      const data = await response.text();
+      return data;
+  } catch (error) {
+      console.error('Error fetching climate data:', error);
+      throw new Error('Failed to fetch climate data');
+  }
+}
+
 
 // Start the server
 app.listen(port, () => {
